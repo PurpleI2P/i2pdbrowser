7fac53d02c06d17357a4ab4af521e179502da4f6
r4sas <r4sas@i2pmail.org>
2016-12-09 01:18:30 +0300

    update to 1.1.1
    Former-commit-id: 5fc7978a382507380ab4c217c265fbaea42b6ffd

--- build/build_7z.bat
+++ build/build_7z.bat
@@ -2,7 +2,7 @@
 
 echo Создаем 7z архив с набором...
 REM На выходе получаем 7z архив, в котором будут лежать папки Firefox, I2Pd и StartI2PdBrowser.bat из папки src уровнем выше.
-7z a -t7z -m0=lzma2 -mx=9 -aoa -mfb=273 -md=512m -ms=on I2PdBrowserPortable_1.1.7z ..\Firefox ..\I2Pd ..\src\StartI2PdBrowser.bat
+7z a -t7z -m0=lzma2 -mx=9 -aoa -mfb=273 -md=512m -ms=on I2PdBrowserPortable_1.1.1.7z ..\Firefox ..\I2Pd ..\src\StartI2PdBrowser.bat
 
 echo Готово!
 pause
--- build/build_exe.bat
+++ build/build_exe.bat
@@ -5,7 +5,9 @@ REM На выходе получаем 7z архив, в котором будут лежать папки Firefox, I2Pd, src
 7z a -t7z -m0=lzma2 -mx=9 -aoa -mfb=273 -md=512m -ms=on I2PdBrowserPortable.7z ..\Firefox ..\I2Pd ..\src ..\StartI2PdBrowser.exe
 
 echo Создаем 7z SFX - самораспаковывающийся архив...
-copy /b 7zsd_LZMA2_i2pdbrowser_1.1.sfx + config.txt + I2PdBrowserPortable.7z I2PdBrowserPortable_1.1.exe >> nul
+REM На выходе получаем самораспаковывающийся архив, архив после этого удаляем.
+copy /b 7zsd_LZMA2_i2pdbrowser_1.1.1.sfx + config.txt + I2PdBrowserPortable.7z I2PdBrowserPortable_1.1.1.exe >> nul
+del I2PdBrowserPortable.7z >> nul
 
 echo Готово!
 pause
