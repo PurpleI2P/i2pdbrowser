7794b9d0d6e6f3e05869e695fa3d7a032d38a9a2
r4sas <r4sas@i2pmail.org>
2017-01-30 04:30:54 +0300

    1.2pre Updated windows bundle builder Added FirefoxPortable.ini from scratch, now it readed correctly
    Former-commit-id: 97b0dd342208bf7c2f9549ea3a184b8e45592f38

---
+++ windows/README.md
@@ -0,0 +1,8 @@
+i2pdbrowser windows
+=====
+Ссылки, откуда были взяты приложения, используемые для сборки i2pdbrowser
+-----
+* 7z: http://7-zip.org/
+* Curl: https://winampplugins.co.uk/curl/
+* CA Root Certificates: https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
+* sed: http://unxutils.sourceforge.net/
--- windows/build/build.cmd
+++ windows/build/build.cmd
@@ -12,7 +12,11 @@ echo.
 echo Загрузка установщика Firefox Portable ESR
 
 "%CURL%" -L -f -# -O https://downloads.sourceforge.net/project/portableapps/Mozilla%%20Firefox%%2C%%20Portable%%20Ed./Mozilla%%20Firefox%%20ESR%%2C%%20Portable%%20Edition%%2045.6.0/FirefoxPortableESR_45.6.0_%locale%.paf.exe %$X%
-if errorlevel 1 (echo ERROR:%ErrorLevel%) else (echo OK!)
+if errorlevel 1 (
+	echo ERROR:%ErrorLevel%
+	pause
+	goto :eof
+) else (echo OK!)
 
 echo.
 echo Распаковка установщика и удаление не нужных файлов
@@ -23,11 +27,12 @@ rmdir /S /Q ..\Firefox\$PLUGINSDIR
 rmdir /S /Q ..\Firefox\App\AppInfo
 rmdir /S /Q ..\Firefox\App\Bin
 rmdir /S /Q ..\Firefox\App\DefaultData\plugins
+rmdir /S /Q ..\Firefox\App\DefaultData\settings
 rmdir /S /Q ..\Firefox\Other
 del /Q ..\Firefox\App\readme.txt
 del /Q ..\Firefox\help.html
 rem if "xOS"=="x86" (
-rem rmdir /S /Q ..\Firefox\App\Firefox64
+rem	rmdir /S /Q ..\Firefox\App\Firefox64
 	rmdir /S /Q ..\Firefox\App\Firefox\dictionaries
 	rmdir /S /Q ..\Firefox\App\Firefox\uninstall
 	del /Q ..\Firefox\App\Firefox\browser\blocklist.xml
@@ -35,7 +40,7 @@ rem rmdir /S /Q ..\Firefox\App\Firefox64
 	del /Q ..\Firefox\App\Firefox\maintenanceservice*.*
 	del /Q ..\Firefox\App\Firefox\update*.*
 rem ) else (
-rem rmdir /S /Q ..\Firefox\App\Firefox
+rem	rmdir /S /Q ..\Firefox\App\Firefox
 	rmdir /S /Q ..\Firefox\App\Firefox64\dictionaries
 	rmdir /S /Q ..\Firefox\App\Firefox64\uninstall
 	del /Q ..\Firefox\App\Firefox64\browser\blocklist.xml
@@ -44,16 +49,30 @@ rem rmdir /S /Q ..\Firefox\App\Firefox
 	del /Q ..\Firefox\App\Firefox64\update*.*
 rem )
 
+echo Отключение отчетов о падении
+rem if "xOS"=="x86" (
+	sed -i "s/Enabled=1/Enabled=0/g" ..\Firefox\App\Firefox\application.ini
+	sed -i "s/ServerURL=.*/ServerURL=-/" ..\Firefox\App\Firefox\application.ini
+	sed -i "s/Enabled=1/Enabled=0/g" ..\Firefox\App\Firefox\webapprt\webapprt.ini
+	sed -i "s/ServerURL=.*/ServerURL=-/" ..\Firefox\App\Firefox\webapprt\webapprt.ini
+rem ) else (
+	sed -i "s/Enabled=1/Enabled=0/g" ..\Firefox\App\Firefox64\application.ini
+	sed -i "s/ServerURL=.*/ServerURL=-/" ..\Firefox\App\Firefox64\application.ini
+	sed -i "s/Enabled=1/Enabled=0/g" ..\Firefox\App\Firefox64\webapprt\webapprt.ini
+	sed -i "s/ServerURL=.*/ServerURL=-/" ..\Firefox\App\Firefox64\webapprt\webapprt.ini
+rem )
+
 echo Загрузка дополнения NoScript
 
 "%CURL%" -L -f -# -O https://addons.mozilla.org/firefox/downloads/latest/noscript/addon-722-latest.xpi
-if errorlevel 1 (echo ERROR:%ErrorLevel%) else (echo OK!)
+if errorlevel 1 (echo ERROR:%ErrorLevel% && pause && goto :eof) else (echo OK!)
 if "xOS"=="x86" (
 	move /Y addon-722-latest.xpi ..\Firefox\App\Firefox\browser\extensions\{73a6fe31-595d-460b-a920-fcc0f8843232}.xpi > nul
 ) else (
 	move /Y addon-722-latest.xpi ..\Firefox\App\Firefox64\browser\extensions\{73a6fe31-595d-460b-a920-fcc0f8843232}.xpi > nul
 )
 
+echo.
 echo Копирование файлов настроек в папку Firefox
 copy /Y profile\* ..\Firefox\App\DefaultData\profile\ > nul
 copy /Y settings\FirefoxPortable.ini ..\Firefox\ > nul
@@ -72,21 +91,22 @@ if "xOS"=="x86" (
 )
 xcopy /E /I /Y i2pd ..\i2pd > nul
 
+echo.
 echo I2Pd Browser portable готов к запуску!
 pause
 exit
 
 :GET_LOCALE
 for /f "tokens=3" %%a in ('reg query "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload"^|find "REG_SZ"') do (
- if %%a==00000419 (set locale=Russian) else (set locale=English)
- goto :eof
+	if %%a==00000419 (set locale=Russian) else (set locale=English)
+	goto :eof
 )
 goto :eof
 
 :GET_PROXY
- set $X=&set $R=HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings
- for /F "Tokens=1,3" %%i in ('reg query "%$R%"^|find "Proxy"') do set %%i=%%j
- if %ProxyEnable%==0x1 set $X=-x %ProxyServer%
+set $X=&set $R=HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings
+for /F "Tokens=1,3" %%i in ('reg query "%$R%"^|find "Proxy"') do set %%i=%%j
+if %ProxyEnable%==0x1 set $X=-x %ProxyServer%
 goto :eof
 
 :GET_ARCH
