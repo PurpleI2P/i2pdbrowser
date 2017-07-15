99d77a934bfc6781e8d14f7718b8d6a40dbccb2f
r4sas <r4sas@i2pmail.org>
2017-07-15 10:10:33 +0300

    remove Batch2EXE config lines

--- windows/StartI2PdBrowser.bat
+++ windows/StartI2PdBrowser.bat
@@ -1,21 +1,4 @@
 @ECHO OFF
-REM BFCPEOPTIONSTART
-REM Advanced BAT to EXE Converter www.BatToExeConverter.com
-REM BFCPEEXE=StartI2PdBrowser.exe
-REM BFCPEICON=i2pd_browser_icon_v3.ico
-REM BFCPEICONINDEX=-1
-REM BFCPEEMBEDDISPLAY=0
-REM BFCPEEMBEDDELETE=1
-REM BFCPEADMINEXE=0
-REM BFCPEINVISEXE=0
-REM BFCPEVERINCLUDE=1
-REM BFCPEVERVERSION=1.2.3.0
-REM BFCPEVERPRODUCT=I2Pd Browser Portable
-REM BFCPEVERDESC=I2Pd Browser
-REM BFCPEVERCOMPANY=PurpleI2P
-REM BFCPEVERCOPYRIGHT=Copyright © 2013-2017 PurpleI2P Project
-REM BFCPEOPTIONEND
-
 REM Copyright (c) 2013-2017, The PurpleI2P Project
 REM This file is part of Purple i2pd project and licensed under BSD3
 REM See full license text in LICENSE file at top of project tree
