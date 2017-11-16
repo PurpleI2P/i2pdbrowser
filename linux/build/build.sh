e2ec884a7bd81556780b6eff8145d1d5fb402cb1
faxboss <faxboss@fedora-23-dvm>
2017-11-16 19:45:24 +0100

    Updated Firefox and I2Pd version in build.cmd and Firefox version in build.sh

--- linux/build/build.sh
+++ linux/build/build.sh
@@ -8,7 +8,7 @@
 
 arch=$(uname -m)
 language=$(echo $LANG | cut -c-5 | sed s/_/-/g)
-version="52.4.1esr"
+version="52.5.0esr"
 application="firefox"
 ftpmirror="https://ftp.mozilla.org/pub/$application/releases/$version"
 
