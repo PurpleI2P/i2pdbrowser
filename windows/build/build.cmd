5f280d98cd98aa908d99fc278ef92120c2c3ac0d
R4SAS <r4sas@i2pmail.org>
2017-11-17 05:53:13 +0300

    PR #9
    Former-commit-id: e698943604704895ce6a50641d740f530eada453

--- windows/build/build.cmd
+++ windows/build/build.cmd
@@ -7,8 +7,8 @@ REM See full license text in LICENSE file at top of project tree
 setlocal enableextensions
 
 set CURL=%~dp0curl.exe
-set FFversion=52.4.1
-set I2Pdversion=2.15.0
+set FFversion=52.5.0
+set I2Pdversion=2.16.0
 call :GET_LOCALE
 call :GET_PROXY
 call :GET_ARCH
