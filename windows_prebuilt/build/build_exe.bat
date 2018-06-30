4eac450fc3d11f493f014cb9ed4fbe3dbb480f07
R4SAS <r4sas@i2pmail.org>
2018-06-30 15:43:52 +0300

    delete one dot in path
    Former-commit-id: 65e72bc454cb91474d3870f05f6decae3739036b

--- windows_prebuilt/build/build_exe.bat
+++ windows_prebuilt/build/build_exe.bat
@@ -6,7 +6,7 @@ REM See full license text in LICENSE file at top of project tree
 
 echo Создаем 7z архив с набором...
 REM На выходе получаем 7z архив, в котором будут лежать папки Firefox, I2Pd, src и файл StartI2PdBrowser.exe уровнем выше.
-7z a -t7z -m0=lzma2:d192m -mx=9 -aoa -mfb=273 -md=128m -ms=on -- I2PdBrowserPortable.7z ..\..\windows\Firefox ..\..\windows\i2pd ...\src\StartI2PdBrowser.bat ..\src\browser.ico
+7z a -t7z -m0=lzma2:d192m -mx=9 -aoa -mfb=273 -md=128m -ms=on -- I2PdBrowserPortable.7z ..\..\windows\Firefox ..\..\windows\i2pd ..\src\StartI2PdBrowser.bat ..\src\browser.ico
 
 echo Создаем 7z SFX - самораспаковывающийся архив...
 REM На выходе получаем самораспаковывающийся архив, архив после этого удаляем.
