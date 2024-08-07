6c313fcd0588c61d0916c4b8ddfe73d7fd740cce
David Uhden Collado <david@uhden.dev>
2024-08-07 18:25:52 +0200

    Update to Firefox 115.14.0esr

--- windows/build/build.cmd
+++ windows/build/build.cmd
@@ -7,7 +7,7 @@ REM See full license text in LICENSE file at top of project tree
 setlocal enableextensions
 
 set CURL=%~dp0curl.exe
-set FFversion=115.13.0esr
+set FFversion=115.14.0esr
 set I2Pdversion=2.53.1
 call :GET_LOCALE
 call :GET_PROXY
