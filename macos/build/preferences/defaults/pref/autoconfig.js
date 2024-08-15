754a824ba507f403f27f28e9f8268641581cb8e2
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2024-08-16 03:38:41 +0700

    personal slimdown

--- macos/build/preferences/defaults/pref/autoconfig.js
+++
@@ -1,2 +0,0 @@
-pref("general.config.filename", "i2pdbrowser.cfg");
-pref("general.config.obscure_value", 0);
--- windows/build/build.cmd
+++ windows/build/build.cmd
@@ -7,23 +7,17 @@ REM See full license text in LICENSE file at top of project tree
 setlocal enableextensions
 
 set CURL=%~dp0curl.exe
-set FFversion=115.14.0esr
+set FFversion=128.1.0esr
 set I2Pdversion=2.53.1
+set locale=en-US
 call :GET_LOCALE
 call :GET_PROXY
 call :GET_ARCH
 
-if "%locale%"=="ru" (
-	echo Сборка I2Pd Browser Portable
-	echo Язык браузера: %locale%, архитектура: %xOS%
-	echo.
-	echo Загрузка установщика Firefox ESR
-) else (
-	echo Building I2Pd Browser Portable
-	echo Browser locale: %locale%, architecture: %xOS%
-	echo.
-	echo Downloading Firefox ESR installer
-)
+echo Building I2Pd Browser Portable
+echo Browser locale: %locale%, architecture: %xOS%
+echo.
+echo Downloading Firefox ESR installer
 
 "%CURL%" -L -f -# -o firefox.exe https://ftp.mozilla.org/pub/firefox/releases/%FFversion%/%xOS%/%locale%/Firefox%%20Setup%%20%FFversion%.exe %$X%
 if errorlevel 1 (
@@ -33,11 +27,8 @@ if errorlevel 1 (
 ) else (echo OK!)
 
 echo.
-if "%locale%"=="ru" (
-	echo Распаковка установщика и удаление не нужных файлов
-) else (
-	echo Unpacking the installer and deleting unnecessary files
-)
+echo Unpacking the installer and deleting unnecessary files
+
 
 7z x -y -o..\Firefox\App firefox.exe > nul
 del /Q firefox.exe
@@ -62,11 +53,7 @@ mkdir ..\Firefox\App\Firefox\browser\extensions > nul
 echo OK!
 
 echo.
-if "%locale%"=="ru" (
-	echo Патчим внутренние файлы браузера для отключения навязчивых запросов
-) else (
-	echo Patching browser internal files to disable annoying external requests
-)
+echo Patching browser internal files to disable annoying external requests
 
 7z -bso0 -y x ..\Firefox\App\Firefox\omni.ja -o..\Firefox\App\tmp > nul 2>&1
 
@@ -88,52 +75,32 @@ del ..\Firefox\App\Firefox\omni.ja.bak
 echo OK!
 
 echo.
-if "%locale%"=="ru" (
-	echo Загрузка языковых пакетов
-) else (
-	echo Downloading language packs
-)
-"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\langpack-ru@firefox.mozilla.org.xpi https://addons.mozilla.org/firefox/downloads/file/4144376/russian_ru_language_pack-115.0.20230726.201356.xpi
-if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
-"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\ruspell-wiktionary@addons.mozilla.org.xpi https://addons.mozilla.org/firefox/downloads/file/4215701/2696307-1.77.xpi
-if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
-"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\langpack-en-US@firefox.mozilla.org.xpi https://addons.mozilla.org/firefox/downloads/file/4144407/english_us_language_pack-115.0.20230726.201356.xpi
+echo Downloading language packs
+
+"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\langpack-en-US@firefox.mozilla.org.xpi https://addons.mozilla.org/firefox/downloads/file/4326091/english_us_language_pack-128.0.20240725.162350.xpi
 if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
 "%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\en-US@dictionaries.addons.mozilla.org.xpi https://addons.mozilla.org/firefox/downloads/file/4175230/us_english_dictionary-115.0.xpi
 if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
 
 echo.
-if "%locale%"=="ru" (
-	echo Загрузка дополнения NoScript
-) else (
-	echo Downloading NoScript extension
-)
-"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\{73a6fe31-595d-460b-a920-fcc0f8843232}.xpi https://addons.mozilla.org/firefox/downloads/file/4320635/noscript-11.4.31.xpi
+echo Downloading NoScript extension
+
+"%CURL%" -L -f -# -o ..\Firefox\App\Firefox\browser\extensions\{73a6fe31-595d-460b-a920-fcc0f8843232}.xpi https://addons.mozilla.org/firefox/downloads/file/4333280/noscript-11.4.34.xpi
 if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
 
 echo.
-if "%locale%"=="ru" (
-	echo Копирование файлов настроек в папку Firefox
-) else (
-	echo Copying Firefox launcher and settings
-)
+echo Copying Firefox launcher and settings
+
 mkdir ..\Firefox\App\DefaultData\profile\ > nul
 xcopy /E /Y profile\* ..\Firefox\App\DefaultData\profile\ > nul
-if "%locale%"=="ru" (
-	copy /Y profile-ru\* ..\Firefox\App\DefaultData\profile\ > nul
-) else (
-	copy /Y profile-en\* ..\Firefox\App\DefaultData\profile\ > nul
-)
+copy /Y profile-en\* ..\Firefox\App\DefaultData\profile\ > nul
 copy /Y firefox-portable\* ..\Firefox\ > nul
 xcopy /E /Y preferences\* ..\Firefox\App\Firefox\ > nul
 echo OK!
 
 echo.
-if "%locale%"=="ru" (
-	echo Загрузка I2Pd
-) else (
-	echo Downloading I2Pd
-)
+echo Downloading I2Pd
+
 "%CURL%" -L -f -# -O https://github.com/PurpleI2P/i2pd/releases/download/%I2Pdversion%/i2pd_%I2Pdversion%_%xOS%_mingw.zip
 if errorlevel 1 ( echo ERROR:%ErrorLevel% && pause && exit ) else (echo OK!)
 7z x -y -o..\i2pd i2pd_%I2Pdversion%_%xOS%_mingw.zip i2pd.exe > nul
@@ -142,20 +109,10 @@ del /Q i2pd_%I2Pdversion%_%xOS%_mingw.zip
 xcopy /E /I /Y i2pd ..\i2pd > nul
 
 echo.
-if "%locale%"=="ru" (
-	echo I2Pd Browser Portable готов к запуску!
-) else (
-	echo I2Pd Browser Portable is ready to start!
-)
+echo I2Pd Browser Portable is ready to start!
+
 pause
 exit
-
-:GET_LOCALE
-for /f "tokens=3" %%a in ('reg query "HKEY_USERS\.DEFAULT\Keyboard Layout\Preload"^|find "REG_SZ"') do (
-	if %%a==00000419 (set locale=ru) else (set locale=en-US)
-	goto :eof
-)
-goto :eof
 
 :GET_PROXY
 set $X=&set $R=HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings
